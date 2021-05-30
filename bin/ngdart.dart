1646425c418b93e0d35baa041c7956ff3585c815
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-05-30 11:42:44 -0400

    Converting template success

--- bin/ngdart.dart
+++ bin/ngdart.dart
@@ -1,11 +1,40 @@
 import 'dart:io';
+import 'dart:convert';
 
-import 'package:ngdart/ngdart_command_runner.dart';
+import 'package:path/path.dart' as p;
+
+import 'package:ngdart/src/templates/new_project.dart';
+import 'package:ngdart/util/conversion.dart';
+
+final _whiteSpace = RegExp(r'\s+');
 
 void main(List<String> args) async {
-  try {
-    exitCode = (await run(args))!;
-  } catch (e) {
-    
+  // try {
+  //   exitCode = (await run(args))!;
+  // } catch (e) {
+
+  // }
+  final data = newProjectTemplateFiles();
+  for (var i = 0; i < data.length; i += 3) {
+    final path = data[i];
+    final type = data[i + 1];
+    final raw = data[i + 2].replaceAll(_whiteSpace, '');
+
+    print('bruh/${path.substring(0, path.length - p.basename(path).length)}');
+
+    final targetDir = Directory(
+        'bruh/${path.substring(0, path.length - p.basename(path).length)}');
+    if (!(await targetDir.exists())) {
+      await targetDir.create(recursive: true);
+    }
+
+    if (type == 'text') {
+      final decoded = Utf8Decoder().convert(base64.decode(raw));
+      await File('bruh/$path')
+          .writeAsString(substituteVars(decoded, {'projectName': 'test'}));
+    } else {
+      final decoded = base64.decode(raw);
+      await File('bruh/$path').writeAsBytes(decoded);
+    }
   }
 }
--- lib/src/commands/create.dart
+++ lib/src/commands/create.dart
@@ -4,48 +4,37 @@ import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 
 class CreateProjectCommand extends Command {
-  final _argParser = ArgParser(allowTrailingOptions: true);
-  ArgParser get argParser => _argParser;
-
-  String readArg(String errorMessage) {
-    var args = argResults?.rest;
-
-    if (args == null || args.isEmpty) {
-      // Usage is provided by command runner.
-      throw UsageException(errorMessage, '');
-    }
-
-    // TODO: I am skeptic about the usefulness of the following code
-    var arg = args.first;
-    args = args.skip(1).toList();
-
-    if (args.isEmpty) {
-      throw UsageException('Unexpected argument $args', '');
-    }
-
-    return arg;
-  }
+  
 
   @override
   // TODO: implement description
-  String get description => throw UnimplementedError();
+  String get description => 'Create a new AngularDart project.';
 
   @override
   String get name => 'create';
+}
 
-  // /// Reads argument for current command and create an EntityName.
-  // EntityName readArgAsEntityName(String errorMessage) =>
-  //     getEntityName(readArg(errorMessage));
 
-  // EntityName getEntityName(String entity) {
-  //   EntityName entityName;
 
-  //   try {
-  //     entityName = new EntityName(entity);
-  //   } on ArgumentError catch (error) {
-  //     throw new UsageException(error.message, '');
+
+  // final _argParser = ArgParser(allowTrailingOptions: true);
+  // ArgParser get argParser => _argParser;
+
+  // String readArg(String errorMessage) {
+  //   var args = argResults?.rest;
+
+  //   if (args == null || args.isEmpty) {
+  //     // Usage is provided by command runner.
+  //     throw UsageException(errorMessage, '');
   //   }
 
-  //   return entityName;
+  //   // TODO: I am skeptic about the usefulness of the following code
+  //   var arg = args.first;
+  //   args = args.skip(1).toList();
+
+  //   if (args.isEmpty) {
+  //     throw UsageException('Unexpected argument $args', '');
+  //   }
+
+  //   return arg;
   // }
-}
