5910f5a01c13aded7704518fb42410aea8bc9da9
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-07-25 11:28:31 -0400

    Add spinner to clean command

--- bin/ngdart.dart
+++ bin/ngdart.dart
@@ -1,25 +1,11 @@
-import 'dart:io';
-
-import 'package:args/command_runner.dart';
-import 'package:logging/logging.dart';
-
 import 'package:ngdart/ngdart_command_runner.dart';
-import 'package:ngdart/util/ansipen.dart';
 import 'package:ngdart/util/logger.dart';
 
 void main(List<String> args) async {
-	// args = ['serve', '-h'];
+  // args = ['serve', '-h'];
 
   var runner = NgdartCommandRunner();
-	CliLogger.initLogger();
+  CliLogger.initLogger();
 
-  try {
-    await runner.run(args);
-  } on UsageException catch (e) {
-    print(errorLog + '$e');
-    exit(64);
-  } catch (e) {
-    print(errorLog + '$e');
-    exit(exitCode);
-  }
+  await runner.run(args);
 }
--- lib/src/commands/clean.dart
+++ lib/src/commands/clean.dart
@@ -2,17 +2,20 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
-import 'package:cli_util/cli_logging.dart';
-
-import 'package:ngdart/util/ansipen.dart';
+import 'package:charcode/charcode.dart';
+import 'package:io/ansi.dart';
+import 'package:interact/interact.dart';
+import 'package:logging/logging.dart';
 
 class CleanCommand extends Command<int> {
+  final logger = Logger('clean');
+
   @override
   String get description => 'Delete the build/ and .dart_tool/ directories.';
 
   @override
   String get name => 'clean';
-  
+
   @override
   String get invocation => 'ngdart clean';
 
@@ -20,34 +23,39 @@ class CleanCommand extends Command<int> {
   FutureOr<int> run() async {
     var buildDir = Directory('build');
     var toolDir = Directory('.dart_tool');
-		var packages = File('.packages');
+    var packages = File('.packages');
     var pubspec = File('pubspec.yaml');
-    var logger = Logger.standard(ansi: Ansi(true));
 
-    try {
-      if (!(await pubspec.exists())) {
-        throw 'pubspec.yaml not found!';
-      }
-      if (await buildDir.exists()) {
-        var progress = logger.progress(progressLog + 'Deleting build');
-        await buildDir.delete(recursive: true);
-        progress.finish(showTiming: true);
-      }
-      if (await toolDir.exists())
-      {
-        var progress = logger.progress(progressLog + 'Deleting .dart_tools');
-        await toolDir.delete(recursive: true);
-        progress.finish(showTiming: true);
-      }
-			if (await packages.exists()) {
-				var progress = logger.progress(progressLog + 'Deleting .packages');
-				await packages.delete();
-				progress.finish(showTiming: true);
-			}
-    } catch (e) {
-      throw Exception(e);
+    if (!(await pubspec.exists())) {
+      logger.severe('pubspec.yaml not found!');
+      throw Exception('pubspec.yaml not found!');
     }
-    print(successLog + 'All clean!');
+    if (await buildDir.exists()) {
+      final build = Spinner(
+        icon: green.wrap('[${String.fromCharCode($radic)}]'),
+        rightPrompt: (done) => 'Deleting build...',
+      ).interact();
+      await buildDir.delete(recursive: true);
+      build.done();
+    }
+    if (await toolDir.exists()) {
+      final tool = Spinner(
+        icon: green.wrap('[${String.fromCharCode($radic)}]'),
+        rightPrompt: (done) => 'Deleting .dart_tools...',
+      ).interact();
+      await toolDir.delete(recursive: true);
+      tool.done();
+    }
+    if (await packages.exists()) {
+      final _packages = Spinner(
+        icon: green.wrap('[${String.fromCharCode($radic)}]'),
+        rightPrompt: (done) => 'Deleting .packages...',
+      ).interact();
+      await packages.delete();
+      _packages.done();
+    }
+
+    logger.info('All clean!');
 
     return 0;
   }
